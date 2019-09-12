0a8c77a693b4c2ee8761e17fc9ed63230dddcee2
anthonyBgood <anthonyBpostbox@gmail.com>
2019-09-12 21:38:23 +1000

    starting app

---
+++ src/components/Land/index.js
@@ -0,0 +1,7 @@
+import Land from 'react';
+const Account = () => (
+  <div>
+    <h1>Account</h1>
+  </div>
+);
+export default Land;
---
+++ src/components/PasswordChange/index.js
@@ -0,0 +1,7 @@
+import PasswordChange from 'react';
+const PasswordChange = () => (
+  <div>
+    <h1>PasswordChange</h1>
+  </div>
+);
+export default PasswordChange;
